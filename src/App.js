b09d01f394472601ec7dee4c9b191dff06a0192d
Sunny Yadav <blinded@list.ru>
2022-06-20 14:01:48 +0300

    Refactor html to jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import PromoText from './components/PromoText'
+import Overview from './components/Overview';
+import Slogan from './components/Slogan';
+import Faq from './components/Faq';
+import Reviews from './components/Reviews'
+import Blog from './components/Blog'
+import Checklist from './components/Checklist';
+import Footer from './components/Footer'
 
 function App() {
+  const promoTexts = [
+    ["Change.It is an exciting online ", "name changing", " service ",
+    " that simplifies the process of how to change your name."],
+    ["We strive to provide the very best in ",
+    "automation", ",", " coupled with a personal, human touch."]
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__container">
+      <div className="app__body">
+        <Header />
+        <PromoText textArray={promoTexts[0]} />
+        <Overview />
+        <Slogan />
+        <Faq />
+        <PromoText textArray={promoTexts[1]}/>
+        <Reviews />
+        <Blog />
+        <Checklist />
+        <Footer />
+      </div>
     </div>
   );
 }
